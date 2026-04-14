bd23e9f475a87251dd0ac6929ad8e6c29821c261
Yatharth Chaudhary <yatharthchaudhary423@gmail.com>
2026-04-14 17:10:06 +0530

    Adding Inorder Traversal

---
+++ trees/Traversal/InorderTraversal.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node *left;
+    node *right;
+
+    node(int d)
+    {
+        this->data = d;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+node *buildTree(node *root)
+{
+    int data;
+
+    cout << "Enter the data : ";
+    cin >> data;
+
+    if (data == -1)
+    {
+        return NULL;
+    }
+    root = new node(data);
+    cout << "Enter the data for inserting in left of " << data << endl;
+    root->left = buildTree(root->left);
+    cout << "Enter the data for inserting in right of " << data << endl;
+    root->right = buildTree(root->right);
+    return root;
+}
+void InorderTraversal(node*root)
+{
+    if(root==NULL)
+    return ;
+    
+    InorderTraversal(root->left);
+    cout<<root->data<<" ";
+    InorderTraversal(root->right);
+}
+int main()
+{
+    node *root = NULL;
+    cout << "Enter the data as -1 for not continuing ." << endl;
+    root = buildTree(root);
+    cout<<"The inorder Traversal : "<<endl;
+    InorderTraversal(root);
+    return 0;
+}
