276bb83c989c133b7496b56aea17fc103de26104
Yatharth Chaudhary <yatharthchaudhary423@gmail.com>
2026-04-18 21:28:24 +0530

    Adding the diameter of the binary tree brute force approach

---
+++ trees/Medium/DiameterOfTheTree.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int count(TreeNode*root)
+    {
+        if(root==NULL)
+        return 0;
+        int MaxLeft=count(root->left);
+        int MaxRight=count(root->right);
+        return max(MaxRight,MaxLeft)+1;
+    }
+    int diameter(TreeNode*root)
+    {
+        if(root==NULL)
+        return 0;
+        int leftpath=count(root->left);
+        int rightpath=count(root->right);
+        int dia=rightpath+leftpath;
+        return max(diameter(root->left), max(diameter(root->right), dia));
+    }
+    int diameterOfBinaryTree(TreeNode* root) {
+        if(root==NULL)
+        return 0;
+        else
+        return diameter(root);
+        
+    }
+}; 
+//This is the brute force to solve this question :taking the height of the left and right of the particular node and adding them and recursively calling for left and right tree asking them for maximum .
+//Time Complexity:O(n*n)
