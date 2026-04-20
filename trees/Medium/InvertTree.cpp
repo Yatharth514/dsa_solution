a1fefccc942c73c909f7a5deda4f764f884b54df
Yatharth Chaudhary <yatharthchaudhary423@gmail.com>
2026-04-20 14:11:05 +0530

    Adding the invert the binary tree code

---
+++ trees/Medium/InvertTree.cpp
@@ -0,0 +1,20 @@
+class Solution
+{
+public:
+    void invert(TreeNode *root)
+    {
+        if (root == NULL)
+            return;
+
+        swap(root->left, root->right);
+        invert(root->left);
+        invert(root->right);
+    }
+    TreeNode *invertTree(TreeNode *root)
+    {
+        if (root == NULL)
+            return root;
+        invert(root);
+        return root;
+    }
+};
