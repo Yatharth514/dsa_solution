8c39547bc0b940c167ed0914debcb1e5709dd373
Yatharth Chaudhary <yatharthchaudhary423@gmail.com>
2026-04-24 17:31:57 +0530

    Adding the simple level question in array folder

---
+++ array/Simple/FurthestPointOfOrigin.cpp
@@ -0,0 +1,30 @@
+class Solution
+{
+public:
+    int furthestDistanceFromOrigin(string moves)
+    {
+        int l = 0;
+        int r = 0;
+        int space = 0;
+        int n = moves.length();
+        for (int i = 0; i < n; i++)
+        {
+            if (moves[i] == 'L')
+                l++;
+            else if (moves[i] == 'R')
+                r++;
+            else
+                space++;
+        }
+        if (l >= r)
+        {
+            l = l + space;
+            return (l - r);
+        }
+        else
+        {
+            r = r + space;
+            return (r - l);
+        }
+    }
+};
