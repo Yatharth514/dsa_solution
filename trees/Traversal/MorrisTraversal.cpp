a6855b0df18b772794a7f43efe8dd46abc9f4487
Yatharth Chaudhary <yatharthchaudhary423@gmail.com>
2026-05-01 19:32:13 +0530

    Adding the morris inorder traversal code

---
+++ trees/Traversal/MorrisTraversal.cpp
@@ -0,0 +1,75 @@
+#include<bits/stdc++.h>
+using namespace std;
+class node
+{
+    public:
+    int data;
+    node* left;
+    node* right;
+    node(int d)
+    {
+        this->data=d;
+        this->left=NULL;
+        this->right=NULL;
+    }
+};
+node *buildTree(node *root)
+{
+    int data;
+
+    cout << "Enter the data : ";
+    cin >> data;
+
+    if (data == -1)
+    {
+        return NULL;
+    }
+    root = new node(data);
+    cout << "Enter the data for inserting in left of " << data << endl;
+    root->left = buildTree(root->left);
+    cout << "Enter the data for inserting in right of " << data << endl;
+    root->right = buildTree(root->right);
+    return root;
+}
+void morris(node* root)
+{
+    node*cur=root;
+    while(cur!=NULL)
+    {
+        if(cur->left==NULL)
+        {
+            cout<<cur->data<<" ";
+            cur=cur->right;
+        }
+        else
+        {
+            node*IP=cur->left;
+            while(IP->right!=NULL&&IP->right!=cur)
+            {
+                IP=IP->right;
+            }
+            if(IP->right==NULL)
+            {
+                IP->right=cur;
+                cur=cur->left;
+            }
+            else
+            {
+                IP->right=NULL;
+                cout<<cur->data<<" ";
+                cur=cur->right;
+            }
+        }
+    }
+    return;
+}
+int main()
+{
+    node *root = NULL;
+    cout << "Enter the data as -1 for not continuing ." << endl;
+    root = buildTree(root);
+    cout<<"The Morris Inorder Traversal : ";
+    morris(root);
+    return 0;
+
+}
