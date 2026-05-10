b2e6bf36d168eb719883608afa88d1d026215702
Yatharth Chaudhary <yatharthchaudhary423@gmail.com>
2026-05-10 22:48:12 +0530

    Adding optimal approach

--- binary_search_tree/Hard/MaximumSumOfABSTinaTree.cpp
+++ binary_search_tree/Hard/MaximumSumOfABSTinaTree.cpp
@@ -36,3 +36,45 @@ public:
 //brute force 
 
 
+
+
+//optimal approach
+
+class Solution {
+public:
+    class info
+    {
+        public:
+        int maxm;
+        int minm;
+        int sum ;
+        bool isValid;
+    };
+    info Max_Sum_BST(TreeNode* root,int &max_sum)
+    {
+        if(!root)
+        {
+            return {INT_MIN,INT_MAX,0,true};
+        }
+        auto left=Max_Sum_BST(root->left,max_sum);
+        auto right=Max_Sum_BST(root->right,max_sum);
+        int cur_sum=0;
+        if(left.isValid&&right.isValid&&left.maxm<root->val&&right.minm>root->val)
+        {
+               cur_sum=root->val+left.sum+right.sum;
+               max_sum=max(max_sum,cur_sum);
+               return {max(root->val,right.maxm),min(root->val,left.minm),cur_sum,true};
+        }
+        else
+        {
+            return {INT_MAX,INT_MIN,0,false};
+        }
+
+    }
+    int maxSumBST(TreeNode* root) {
+        int max_sum=0;
+        Max_Sum_BST(root,max_sum);
+        return max_sum;
+        
+    }
+};
