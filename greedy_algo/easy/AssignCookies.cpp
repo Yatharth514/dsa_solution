393de6df846b12340d0b666e044844adbc12a050
Yatharth Chaudhary <yatharthchaudhary423@gmail.com>
2026-05-25 02:30:06 +0530

    Starting greedy problems

---
+++ greedy_algo/easy/AssignCookies.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        int n = g.size();
+        int m = s.size();
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
+        int c = 0;
+        int i = 0;
+        int j = 0;
+        while (i < n && j < m) {
+            if (s[j] >= g[i]) {
+                c++;
+                j++;
+                i++;
+            } else
+                j++;
+        }
+        return c;
+    }
+};
